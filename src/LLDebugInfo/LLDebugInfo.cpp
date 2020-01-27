f254831f836ba78120a33f9505adf4eafde540cf
Abdul Dakkak <abduld@wolfram.com>
2020-01-27 11:59:26 -0600

    update to llvm 9

--- src/LLDebugInfo/LLDebugInfo.cpp
+++ src/LLDebugInfo/LLDebugInfo.cpp
@@ -11,23 +11,20 @@
 
 #include "LLDebugInfo.h"
 
-#include <llvm/Support/raw_ostream.h>
 #include <llvm/IR/Instructions.h>
+#include <llvm/Support/raw_ostream.h>
 
 using namespace llvm;
 
 LLDebugInfo::LLDebugInfo(llvm::Module *M, StringRef Filename, StringRef Directory)
-        : M(M), DIB(*M) {
-  M->addModuleFlag(Module::Warning, "Debug Info Version", (uint32_t)DEBUG_METADATA_VERSION);
+    : M(M), DIB(*M) {
+  M->addModuleFlag(
+      Module::Warning, "Debug Info Version", (uint32_t) DEBUG_METADATA_VERSION);
   M->addModuleFlag(Module::Warning, "Dwarf Version", 4);
 
   DIFile *DebugSrcFile = DIB.createFile(Filename, Directory);
-  CU = DIB.createCompileUnit(dwarf::DW_LANG_lo_user,
-                             DebugSrcFile,
-                             "llvm-dbas",
-                             false,
-                             "",
-                             0);
+  CU                   = DIB.createCompileUnit(
+      dwarf::DW_LANG_lo_user, DebugSrcFile, "llvm-dbas", false, "", 0);
 
   File = CU->getFile();
 }
@@ -35,71 +32,103 @@ LLDebugInfo::LLDebugInfo(llvm::Module *M, StringRef Filename, StringRef Director
 DIType *LLDebugInfo::addType(Type *Ty, llvm::Module *Mod) {
   DataLayout layout = DataLayout(Mod);
   switch (Ty->getTypeID()) {
-  case Type::HalfTyID:
-    return DIB.createBasicType("half", 16, dwarf::DW_ATE_float);
-  case Type::FloatTyID:
-    return DIB.createBasicType("float", 32, dwarf::DW_ATE_float);
-  case Type::DoubleTyID:
-    return DIB.createBasicType("double", 64, dwarf::DW_ATE_float);
-  case Type::X86_FP80TyID:
-    return DIB.createBasicType("x86_fp80", 80, dwarf::DW_ATE_float);
-  case Type::FP128TyID:
-    return DIB.createBasicType("fp128", 128, dwarf::DW_ATE_float);
-  case Type::PPC_FP128TyID:
-    return DIB.createBasicType("ppc_fp128", 128, dwarf::DW_ATE_float);
-  case Type::VoidTyID:
-  case Type::LabelTyID:
-  case Type::MetadataTyID:
-  case Type::X86_MMXTyID:
-  case Type::VectorTyID:
-  case Type::TokenTyID: {
-    std::string name;
-    raw_string_ostream os(name);
-    Ty->print(os);
-    return DIB.createUnspecifiedType(name);
-  }
-  case Type::IntegerTyID: {
-    IntegerType *IntTy = dyn_cast<IntegerType>(Ty);
-    return DIB.createBasicType("i" + std::to_string(IntTy->getBitWidth()), IntTy->getBitWidth(), dwarf::DW_ATE_unsigned);
-  }
-  case Type::FunctionTyID: {
-    FunctionType *FuncTy = dyn_cast<FunctionType>(Ty);
-    std::vector<Metadata *> argTypes;
-    for (Type *ArgType : FuncTy->params()) {
-      argTypes.push_back(addType(ArgType, Mod));
+    case Type::HalfTyID:
+      return DIB.createBasicType("half", 16, dwarf::DW_ATE_float);
+    case Type::FloatTyID:
+      return DIB.createBasicType("float", 32, dwarf::DW_ATE_float);
+    case Type::DoubleTyID:
+      return DIB.createBasicType("double", 64, dwarf::DW_ATE_float);
+    case Type::X86_FP80TyID:
+      return DIB.createBasicType("x86_fp80", 80, dwarf::DW_ATE_float);
+    case Type::FP128TyID:
+      return DIB.createBasicType("fp128", 128, dwarf::DW_ATE_float);
+    case Type::PPC_FP128TyID:
+      return DIB.createBasicType("ppc_fp128", 128, dwarf::DW_ATE_float);
+    case Type::VoidTyID:
+    case Type::LabelTyID:
+    case Type::MetadataTyID:
+    case Type::X86_MMXTyID:
+    case Type::VectorTyID:
+    case Type::TokenTyID: {
+      std::string name;
+      raw_string_ostream os(name);
+      Ty->print(os);
+      return DIB.createUnspecifiedType(name);
     }
-    return DIB.createSubroutineType(DIB.getOrCreateTypeArray(argTypes));
-  }
-  case Type::StructTyID: {
-    StructType *StructTy = dyn_cast<StructType>(Ty);
-    if (StructTy->isOpaque()) {
-      return DIB.createUnspecifiedType(StructTy->getName());
+    case Type::IntegerTyID: {
+      IntegerType *IntTy = dyn_cast<IntegerType>(Ty);
+      return DIB.createBasicType("i" + std::to_string(IntTy->getBitWidth()),
+                                 IntTy->getBitWidth(),
+                                 dwarf::DW_ATE_unsigned);
     }
-    std::vector<Metadata *> argTypes;
-    int i = 0;
-    for (Type *ArgType : StructTy->elements()) {
-      DIType *DeclTy = addType(ArgType, Mod);
-      argTypes.push_back(DIB.createMemberType(File, std::to_string(i), File, 0, layout.getTypeSizeInBits(ArgType), layout.getABITypeAlignment(ArgType), layout.getStructLayout(StructTy)->getElementOffsetInBits(i), DINode::FlagZero, DeclTy));
-      i++;
+    case Type::FunctionTyID: {
+      FunctionType *FuncTy = dyn_cast<FunctionType>(Ty);
+      std::vector<Metadata *> argTypes;
+      for (Type *ArgType : FuncTy->params()) {
+        argTypes.push_back(addType(ArgType, Mod));
+      }
+      return DIB.createSubroutineType(DIB.getOrCreateTypeArray(argTypes));
     }
-    return DIB.createStructType(File, StructTy->hasName() ? StructTy->getName() : "", File, 0, layout.getTypeSizeInBits(Ty), layout.getABITypeAlignment(Ty), DINode::FlagZero, nullptr, DIB.getOrCreateArray(argTypes));
-  }
-  case Type::ArrayTyID: {
-    ArrayType *ArrayTy = dyn_cast<ArrayType>(Ty);
-    std::vector<Metadata *> subscripts;
-    subscripts.push_back(DIB.getOrCreateSubrange(0, ArrayTy->getNumElements()));
-    return DIB.createArrayType(layout.getTypeSizeInBits(Ty), layout.getABITypeAlignment(Ty), addType(ArrayTy->getElementType(), Mod), DIB.getOrCreateArray(subscripts));
-  }
-  case Type::PointerTyID: {
-    PointerType *PointerTy = dyn_cast<PointerType>(Ty);
-    return DIB.createPointerType(addType(PointerTy->getElementType(), Mod), layout.getTypeSizeInBits(Ty));
-  }
+    case Type::StructTyID: {
+      StructType *StructTy = dyn_cast<StructType>(Ty);
+      if (StructTy->isOpaque()) {
+        return DIB.createUnspecifiedType(StructTy->getName());
+      }
+      std::vector<Metadata *> argTypes;
+      int i = 0;
+      for (Type *ArgType : StructTy->elements()) {
+        DIType *DeclTy = addType(ArgType, Mod);
+        argTypes.push_back(DIB.createMemberType(
+            File,
+            std::to_string(i),
+            File,
+            0,
+            layout.getTypeSizeInBits(ArgType),
+            layout.getABITypeAlignment(ArgType),
+            layout.getStructLayout(StructTy)->getElementOffsetInBits(i),
+            DINode::FlagZero,
+            DeclTy));
+        i++;
+      }
+      return DIB.createStructType(File,
+                                  StructTy->hasName() ? StructTy->getName() : "",
+                                  File,
+                                  0,
+                                  layout.getTypeSizeInBits(Ty),
+                                  layout.getABITypeAlignment(Ty),
+                                  DINode::FlagZero,
+                                  nullptr,
+                                  DIB.getOrCreateArray(argTypes));
+    }
+    case Type::ArrayTyID: {
+      ArrayType *ArrayTy = dyn_cast<ArrayType>(Ty);
+      std::vector<Metadata *> subscripts;
+      subscripts.push_back(DIB.getOrCreateSubrange(0, ArrayTy->getNumElements()));
+      return DIB.createArrayType(layout.getTypeSizeInBits(Ty),
+                                 layout.getABITypeAlignment(Ty),
+                                 addType(ArrayTy->getElementType(), Mod),
+                                 DIB.getOrCreateArray(subscripts));
+    }
+    case Type::PointerTyID: {
+      PointerType *PointerTy = dyn_cast<PointerType>(Ty);
+      return DIB.createPointerType(addType(PointerTy->getElementType(), Mod),
+                                   layout.getTypeSizeInBits(Ty));
+    }
   }
 }
 
 DISubprogram *LLDebugInfo::addFunction(Function *F, unsigned int Line) {
   DISubroutineType *ST = dyn_cast<DISubroutineType>(addType(F->getFunctionType(), M));
-  DISubprogram *SP = DIB.createFunction(File, F->getName(), F->getName(), File, Line, ST, true, true, 0);
+  DISubprogram *SP =
+      DIB.createFunction(File,
+                         F->getName(),
+                         F->getName(),
+                         File,
+                         Line,
+                         ST,
+                         true,
+                         DINode::FlagZero,
+                         DISubprogram::SPFlagDefinition | DISubprogram::SPFlagOptimized);
   F->setSubprogram(SP);
   return SP;
 }
@@ -108,15 +137,23 @@ void LLDebugInfo::addParameters(Function *F, DISubprogram *SP, unsigned int Line
   int i = 0;
   for (Argument &arg : F->args()) {
     DIType *Ty = addType(arg.getType(), M);
-    DILocalVariable *V = DIB.createParameterVariable(SP, arg.getName(), i++, File, Line, Ty);
-    DIB.insertDbgValueIntrinsic(&arg, 0, V, DIB.createExpression(), DebugLoc::get(Line, 0, SP).get(), F->getEntryBlock().getFirstNonPHI());
+    DILocalVariable *V =
+        DIB.createParameterVariable(SP, arg.getName(), i++, File, Line, Ty);
+    DIB.insertDeclare(&arg,
+                      V,
+                      DIB.createExpression(),
+                      DebugLoc::get(Line, 0, SP).get(),
+                      F->getEntryBlock().getFirstNonPHI());
   }
-
 }
 
-void LLDebugInfo::addInstruction(Instruction *I, DISubprogram *SP, std::string NameStr, int NameID, unsigned int Line) {
+void LLDebugInfo::addInstruction(Instruction *I,
+                                 DISubprogram *SP,
+                                 std::string NameStr,
+                                 int NameID,
+                                 unsigned int Line) {
   std::string name = NameStr.empty() ? std::to_string(NameID) : NameStr;
-  DebugLoc loc = DebugLoc::get(Line, 0, SP);
+  DebugLoc loc     = DebugLoc::get(Line, 0, SP);
   I->setDebugLoc(loc);
   if (!isa<PHINode>(I) && !I->isEHPad()) {
     for (auto i : DbgValues) {
@@ -125,9 +162,10 @@ void LLDebugInfo::addInstruction(Instruction *I, DISubprogram *SP, std::string N
     DbgValues.clear();
   }
   if (!I->getType()->isVoidTy()) {
-    DIType *Ty = addType(I->getType(), M);
+    DIType *Ty         = addType(I->getType(), M);
     DILocalVariable *V = DIB.createAutoVariable(SP, name, File, Line, Ty);
-    auto inst = DIB.insertDbgValueIntrinsic(I, 0, V, DIB.createExpression(), loc.get(), I->getParent());
+    auto inst =
+        DIB.insertDeclare(I, V, DIB.createExpression(), loc.get(), I->getParent());
     inst->removeFromParent();
     DbgValues.push_back(inst);
   }
--- src/main.cpp
+++ src/main.cpp
@@ -30,91 +30,93 @@
 
 using namespace llvm;
 
-static cl::opt<std::string> InputFilename(cl::Positional, cl::desc("<input .ll file>"), cl::init("-"));
+static cl::opt<std::string> InputFilename(cl::Positional, cl::desc("<input .ll file>"),
+                                          cl::init("-"));
 
-static cl::opt<std::string> OutputFilename("o", cl::desc("Override output filename"), cl::value_desc("filename"));
+static cl::opt<std::string> OutputFilename("o", cl::desc("Override output filename"),
+                                           cl::value_desc("filename"));
 
 static cl::opt<bool> Force("f", cl::desc("Enable binary output on terminals"));
 
-static cl::opt<bool> DisableOutput("disable-output", cl::desc("Disable output"), cl::init(false));
+static cl::opt<bool> DisableOutput("disable-output", cl::desc("Disable output"),
+                                   cl::init(false));
 
-static cl::opt<bool> EmitModuleHash("module-hash", cl::desc("Emit module hash"), cl::init(false));
+static cl::opt<bool> EmitModuleHash("module-hash", cl::desc("Emit module hash"),
+                                    cl::init(false));
 
 static cl::opt<bool> DumpAsm("d", cl::desc("Print assembly as parsed"), cl::Hidden);
 
-static cl::opt<bool> DisableVerify("disable-verify", cl::Hidden, cl::desc("Do not run verifier on input LLVM (dangerous!)"));
+static cl::opt<bool>
+    DisableVerify("disable-verify", cl::Hidden,
+                  cl::desc("Do not run verifier on input LLVM (dangerous!)"));
 
-static cl::opt<bool> PreserveBitcodeUseListOrder("preserve-bc-uselistorder", cl::desc("Preserve use-list order when writing LLVM bitcode."), cl::init(true), cl::Hidden);
+static cl::opt<bool> PreserveBitcodeUseListOrder(
+    "preserve-bc-uselistorder",
+    cl::desc("Preserve use-list order when writing LLVM bitcode."), cl::init(true),
+    cl::Hidden);
 
-static void WriteOutputFile(const Module *M)
-{
-	// Infer the output filename if needed.
-	if (OutputFilename.empty())
-	{
-		if (InputFilename == "-")
-		{
-			OutputFilename = "-";
-		}
-		else
-		{
-			StringRef IFN = InputFilename;
-			OutputFilename = (IFN.endswith(".ll") ? IFN.drop_back(3) : IFN).str();
-			OutputFilename += ".bc";
-		}
-	}
+static void WriteOutputFile(const Module *M) {
+  // Infer the output filename if needed.
+  if (OutputFilename.empty()) {
+    if (InputFilename == "-") {
+      OutputFilename = "-";
+    } else {
+      StringRef IFN  = InputFilename;
+      OutputFilename = (IFN.endswith(".ll") ? IFN.drop_back(3) : IFN).str();
+      OutputFilename += "_debug.bc";
+    }
+  }
 
-	std::error_code EC;
-	std::unique_ptr<tool_output_file> Out(new tool_output_file(OutputFilename, EC, sys::fs::F_None));
-	if (EC)
-	{
-		errs() << EC.message() << '\n';
-		exit(1);
-	}
+  std::error_code EC;
+  std::unique_ptr<ToolOutputFile> Out(
+      new ToolOutputFile(OutputFilename, EC, sys::fs::F_None));
+  if (EC) {
+    errs() << EC.message() << '\n';
+    exit(1);
+  }
 
-	if (Force || !CheckBitcodeOutputToConsole(Out->os(), true))
-		WriteBitcodeToFile(M, Out->os(), PreserveBitcodeUseListOrder, nullptr,
-				           EmitModuleHash);
+  if (Force || !CheckBitcodeOutputToConsole(Out->os(), true))
+    WriteBitcodeToFile(*M, Out->os(), PreserveBitcodeUseListOrder, nullptr,
+                       EmitModuleHash);
 
-	// Declare success.
-	Out->keep();
+  // Declare success.
+  Out->keep();
 }
 
-int main(int argc, char **argv)
-{
-	// Print a stack trace if we signal out.
-	sys::PrintStackTraceOnErrorSignal(argv[0]);
-	PrettyStackTraceProgram X(argc, argv);
-	LLVMContext Context;
-	llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
-	cl::ParseCommandLineOptions(argc, argv, "llvm .ll -> .bc assembler adding debug information at IR level\n");
+int main(int argc, char **argv) {
+  // Print a stack trace if we signal out.
+  sys::PrintStackTraceOnErrorSignal(argv[0]);
+  PrettyStackTraceProgram X(argc, argv);
+  LLVMContext Context;
+  llvm_shutdown_obj Y; // Call llvm_shutdown() on exit.
+  cl::ParseCommandLineOptions(
+      argc, argv, "llvm .ll -> .bc assembler adding debug information at IR level\n");
 
-	// Parse the file now...
-	SMDiagnostic Err;
-	std::unique_ptr<Module> M = parseAssemblyFile(InputFilename, Err, Context);
-	if (!M.get())
-	{
-		Err.print(argv[0], errs());
-		return 1;
-	}
+  // Parse the file now...
+  SMDiagnostic Err;
+  std::unique_ptr<Module> M = parseAssemblyFile(InputFilename, Err, Context);
+  if (!M.get()) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
 
-	if (!DisableVerify)
-	{
-		std::string ErrorStr;
-		raw_string_ostream OS(ErrorStr);
-		if (verifyModule(*M.get(), &OS)) {
-			errs() << argv[0]
-				   << ": assembly parsed, but does not verify as correct!\n";
-			errs() << OS.str();
-			return 1;
-		}
-	}
+  if (!DisableVerify) {
+    std::string ErrorStr;
+    raw_string_ostream OS(ErrorStr);
+    if (verifyModule(*M.get(), &OS)) {
+      errs() << argv[0] << ": assembly parsed, but does not verify as correct!\n";
+      errs() << OS.str();
+      return 1;
+    }
+  }
 
-	//ProcessModule(M.get());
+  // ProcessModule(M.get());
 
-	if (DumpAsm) errs() << "Here's the assembly:\n" << *M.get();
+  if (DumpAsm)
+    errs() << "Here's the assembly:\n" << *M.get();
 
-	if (!DisableOutput)
-		WriteOutputFile(M.get());
+  if (!DisableOutput)
+    WriteOutputFile(M.get());
 
-	return 0;
+  return 0;
 }
